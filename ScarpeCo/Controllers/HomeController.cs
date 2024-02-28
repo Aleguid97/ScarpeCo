62b69d84f90936429db1542567b015ef1869ae49
Aleguid97 <aleguid97@gmail.com>
2024-02-28 16:55:34 +0100

    Aggiungere i file di progetto.

---
+++ ScarpeCo/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using ScarpeCo.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScarpeCo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Scarpe> scarpe = new List<Scarpe>();
+            string connectionString = ConfigurationManager.ConnectionStrings["Scarpe"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "SELECT * FROM SCARPE";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Scarpe d = new Scarpe();
+                        d.IdArticolo = Convert.ToInt16(reader["IdArticolo"]);
+                        d.Nome = reader["Nome"].ToString();
+                        d.Descrizione = reader["Descrizione"].ToString();
+                        d.Prezzo = Convert.ToDecimal(reader["Prezzo"]);
+                        d.ImgPath = reader["ImgPath"].ToString();
+                        d.ImgAlt1 = reader["ImgAlt1"].ToString();
+                        d.ImgAlt2 = reader["ImgAlt2"].ToString();
+                        d.Visibile = Convert.ToBoolean(reader["Visibile"]);
+                        scarpe.Add(d);
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("Errore: " + ex.Message);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+                return View(scarpe);
+            }
+
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ScarpeCo/Models/Scarpe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScarpeCo.Models
+{
+    public class Scarpe
+    {
+            public int IdArticolo { get; set; }
+            public string Nome { get; set; }
+            public string Descrizione { get; set; }
+            public decimal Prezzo { get; set; }
+            public string ImgPath { get; set; }
+            public string  ImgAlt1 { get; set; }
+            public string ImgAlt2 { get; set; }
+            public bool Visibile { get; set; }
+
+       
+    }
+}
